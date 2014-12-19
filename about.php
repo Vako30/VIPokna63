e37323d63da3d30c2f906e76f2eeaa4305d5fd4b
Smarter1 <vako3008@gmail.com>
2014-12-19 19:02:05 +0400

    about

--- about.php
+++ about.php
@@ -5,15 +5,15 @@ Template Name: about
 ?>
 <div id="page">
     <div class="content">
-        <div class="field-item even col-xs-12" property="content:encoded">
-            <div class="out out-pattern" style="opacity: 1;">
+        <div class="field-item even" property="content:encoded">
+            <div class="out out-pattern col-xs-12" style="background:#fff; margin:10px 0; border-radius:4px; padding:15px">
 
                 <div class="rtejustify " style="font-family:arial,helvetica,sans-serif;font-size:20px;">
                     <span style="color:#ff0000; font-size:24px;font-weight:bold;">«VIP-ОКНА»</span>
                     — это производитель окон, который начал свое развитие в 1993 в Армении,
                     и два года назад начали свою деятельность в городе Самара. За это время
                     мы успели заслужить доверие и уважение многих клиентов.
-                    <span style="color:#ff0000;font-weight:bold;"> Мы работаем качественно, выполняя даже самые сложные заказы точно в срок.</span>
+                    <span style="color:#ff0000;font-weight:bold;"> Мы работаем качественно, выполняя даже самые сложные заказы точно в срок.</span><br><br>
                 </div>
                 <div class="rtejustify" style="font-family:arial,helvetica,sans-serif;font-size:20px;">
                     Пластиковые окна на сегодняшний день являются
@@ -28,36 +28,41 @@ Template Name: about
                     службы даже идеально изготовленного окна. 
                 </div>
             </div>
+            <div style="clear:both"></div>
 
-            <div class="twocolumns"  style="margin: 0px 0px 20px; padding: 0px; color: rgb(61, 61, 64); font-family: Calibri, Helvetica, Arial; font-size: 20px; line-height: 24px;">
-                <h3 class="h1" style="text-align:center; color:#ff0000">Мы рады предложить Вам</h3>
-                <ul style="padding-right: 0px; padding-left: 0px; list-style: none; zoom: 1;">
-                    <li class="col-md-6 col-xs-12 icons-li">
-                        <strong><span style="color:#000000;">
+            <div class="panel panel-danger" style="font-family: Calibri, Helvetica, Arial; font-size: 18px; margin-top:20px">
+                <div class="panel-heading">
+                    <h3 class="panel-title" style="text-align:center; font-size: 22px">Мы рады предложить Вам</h3>
+                </div>
+                <div class="panel-body">
+                    <ul style="padding-right: 0px; padding-left: 0px; list-style: none; zoom: 1;">
+                        <li class="col-md-6 col-xs-12 icons-li">
+                            <span style="color:#000000;">
                                 <img style="margin-right:15px"
-                                                                  src="http://www.mosokna.ru/bitrix/templates/mosokna/images/icon_feat_photo.png"/>Современные пластиковые окна и балконные конструкции </span><a
-                                href="http://www.mosokna.ru/ceny/"
-                                style="margin: 0px; padding: 0px; color: rgb(51, 51, 51);"><span
-                                    style="color:#000000;">по доступным ценам</span></a><span
-                                style="color:#000000;">.</span></strong>
-                    </li>
-                    <li  class="col-md-6 col-xs-12 icons-li">
-                        <strong><span style="color:#000000;">
-                                <img style="margin-right:15px" src="http://www.mosokna.ru/bitrix/templates/mosokna/images/icon_feat_star.png"/>Квалифицированный подход к решению задач любой сложности.</span></strong>
-                    </li>
-                    <li class="col-md-6 col-xs-12 icons-li">
-                        <strong><span style="color:#000000;">
+                                     src="http://www.mosokna.ru/bitrix/templates/mosokna/images/icon_feat_photo.png"/>Современные пластиковые окна и балконные конструкции </span><a
+                                    href="http://www.mosokna.ru/ceny/"
+                                    style="margin: 0px; padding: 0px; color: rgb(51, 51, 51);"><span
+                                        style="color:#000000;">по доступным ценам</span></a><span
+                                    style="color:#000000;">.</span>
+                        </li>
+                        <li  class="col-md-6 col-xs-12 icons-li">
+                            <span style="color:#000000;">
+                                <img style="margin-right:15px" src="http://www.mosokna.ru/bitrix/templates/mosokna/images/icon_feat_star.png"/>Квалифицированный подход к решению задач любой сложности.</span>
+                        </li>
+                        <li class="col-md-6 col-xs-12 icons-li">
+                            <span style="color:#000000;">
                                 <img style="margin-right:15px"  src="http://www.mosokna.ru/bitrix/templates/mosokna/images/icon_feat_brush.png" />Оперативный </span><a
-                                href="http://www.mosokna.ru/uslugi/montaj/" ><span
-                                    style="color:#000000;">монтаж пластиковых окон по ГОСТу</span></a><span
-                                style="color:#000000;">, который выполняется специалистами с солидным опытом.</span></strong>
-                    </li>
-                    <li class="col-md-6 col-xs-12 icons-li">
-                        <strong><span style="color:#000000;">
+                                    href="http://www.mosokna.ru/uslugi/montaj/" ><span
+                                        style="color:#000000;">монтаж пластиковых окон по ГОСТу</span></a><span
+                                    style="color:#000000;">, который выполняется специалистами с солидным опытом.</span>
+                        </li>
+                        <li class="col-md-6 col-xs-12 icons-li">
+                            <span style="color:#000000;">
                             <img style="margin-right:15px" src="http://www.mosokna.ru/bitrix/templates/mosokna/images/icon_feat_like.png"/></span>
-                            <a href="http://www.mosokna.ru/info/garantiya/" style="margin: 0px; padding: 0px; color: rgb(153, 153, 153);"><span style="color:#000000;">Гарантию высочайшего качестванашей продукции.</span></a></strong>
-                    </li>
-                </ul>
+                                <a href="http://www.mosokna.ru/info/garantiya/" style="margin: 0px; padding: 0px; color: rgb(153, 153, 153);"><span style="color:#000000;">Гарантию высочайшего качестванашей продукции.</span></a>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </div>
         <!-- /main-content -->
