6b5a5b9481004eec9b06c05748de2329ebd96d4d
Smarter1 <vako3008@gmail.com>
2014-12-19 18:31:36 +0400

    about

--- about.php
+++ about.php
@@ -4,137 +4,64 @@ Template Name: about
 */
 ?>
 <div id="page">
-    <div id="columns">
-        <div id="mainContent">
-            <div class="local-tasks"></div>
-            <div id="content">
-                <div id="block-system-main" class="block block-system">
-                        <div class="block-inner">
-                            <div class="content">
-                                <div id="node-6" class="node node-page" about="/about" typeof="foaf:Document">
-                                    <div class="node-inner clearfix">
-                                        <div class="node-content">
-                                            <div class="field field-name-body field-type-text-with-summary field-label-hidden">
-                                                <div class="field-item even col-xs-12" property="content:encoded">
-                                                    <div class="out out-pattern" style="opacity: 1;">
+    <div class="content">
+        <div class="field-item even col-xs-12" property="content:encoded">
+            <div class="out out-pattern" style="opacity: 1;">
 
-                                                                   <div class="rtejustify " style="font-family:arial,helvetica,sans-serif;font-size:20px;">
-                                                                       <span style="color:#ff0000; font-size:24px;font-weight:bold;">«VIP-ОКНА»</span>
-                                                                       — это производитель окон, который начал свое развитие в 1993 в Армении,
-                                                                    и два года назад начали свою деятельность в городе Самара. За это время
-                                                                    мы успели заслужить доверие и уважение многих клиентов.
-                                                                    <span style="color:#ff0000;font-weight:bold;"> Мы работаем качественно, выполняя даже самые сложные заказы точно в срок.</span>
-                                                                    </div>
-                                                                <div class="rtejustify" style="font-family:arial,helvetica,sans-serif;font-size:20px;">
-                                                                        Пластиковые окна на сегодняшний день являются
-                                                                        незаменимым элементом дизайнерских решений, приносящим в дом
-                                                                        солнечный свет, тепло и уют. На рынке оконной индустрии
-                                                                        представлен широкий выбор разноплановых оконных конструкций.
+                <div class="rtejustify " style="font-family:arial,helvetica,sans-serif;font-size:20px;">
+                    <span style="color:#ff0000; font-size:24px;font-weight:bold;">«VIP-ОКНА»</span>
+                    — это производитель окон, который начал свое развитие в 1993 в Армении,
+                    и два года назад начали свою деятельность в городе Самара. За это время
+                    мы успели заслужить доверие и уважение многих клиентов.
+                    <span style="color:#ff0000;font-weight:bold;"> Мы работаем качественно, выполняя даже самые сложные заказы точно в срок.</span>
+                </div>
+                <div class="rtejustify" style="font-family:arial,helvetica,sans-serif;font-size:20px;">
+                    Пластиковые окна на сегодняшний день являются
+                    незаменимым элементом дизайнерских решений, приносящим в дом
+                    солнечный свет, тепло и уют. На рынке оконной индустрии
+                    представлен широкий выбор разноплановых оконных конструкций.
 
-                                                                    <span style="color:#ff0000;font-size:20px;font-family:arial,helvetica,sans-serif;">Мы не устанавливаем дешевых окон.</span>
-                                                                         Только надёжные, проверенные марки,
-                                                                        но по приемлемым ценам. И не стоит забывать о технологии
-                                                                        установки окон ПВХ - неправильный монтаж сокращает срок
-                                                                        службы даже идеально изготовленного окна. 
-                                                                </div>
-                                                    </div>
-                                                   <p style="color: rgb(61, 61, 64); font-family: Calibri, Helvetica, Arial; font-size: 20px; line-height: 24px;">
-                                                        <strong>
-                                                            <span style="color:#000000;font-size:20px;">Мы рады предложить Вам:</span>
-                                                        </strong>
-                                                    </p>
+                    <span style="color:#ff0000;font-size:20px;font-family:arial,helvetica,sans-serif;">Мы не устанавливаем дешевых окон.</span>
+                    Только надёжные, проверенные марки,
+                    но по приемлемым ценам. И не стоит забывать о технологии
+                    установки окон ПВХ - неправильный монтаж сокращает срок
+                    службы даже идеально изготовленного окна. 
+                </div>
+            </div>
 
-
-                                                    <div class="twocolumns"  style="margin: 0px 0px 20px; padding: 0px; color: rgb(61, 61, 64); font-family: Calibri, Helvetica, Arial; font-size: 20px; line-height: 24px;">
-                                                        <ul style="margin: 28px -10px 20px -50px; padding-right: 0px; padding-left: 0px; list-style: none; zoom: 1;">
-                                                            <li class="col-md-6 col-xs-12 icons-li">
-                                                                <strong><span style="color:#000000;"><img style="margin: 0px 16px 0px -45px; padding: 0px;"
-                                                                                                          src="http://www.mosokna.ru/bitrix/templates/mosokna/images/icon_feat_photo.png"/>Современные пластиковые окна и балконные конструкции </span><a
-                                                                        href="http://www.mosokna.ru/ceny/"
-                                                                        style="margin: 0px; padding: 0px; color: rgb(51, 51, 51);"><span
-                                                                            style="color:#000000;">по доступным ценам</span></a><span
-                                                                        style="color:#000000;">.</span></strong>
-                                                            </li>
-                                                            <li  class="col-md-6 col-xs-12 icons-li">
-                                                                <strong><span style="color:#000000;"><img style="margin: 0px 16px 0px -45px; padding: 0px;"
-                                                                                                          src="http://www.mosokna.ru/bitrix/templates/mosokna/images/icon_feat_star.png"/>Квалифицированный подход к решению задач любой сложности.</span></strong>
-                                                            </li>
-                                                            <li class="col-md-6 col-xs-12 icons-li"  class="col-md-6 col-xs-12 icons-li">
-                                                                <strong><span style="color:#000000;"><img style="margin: 0px 16px 0px -45px; padding: 0px;"  src="http://www.mosokna.ru/bitrix/templates/mosokna/images/icon_feat_brush.png" />Оперативный </span><a
-                                                                        href="http://www.mosokna.ru/uslugi/montaj/"
-                                                                        style="margin: 0px; padding: 0px; color: rgb(51, 51, 51);"><span
-                                                                            style="color:#000000;">монтаж пластиковых окон по ГОСТу</span></a><span
-                                                                        style="color:#000000;">, который выполняется специалистами с солидным опытом.</span></strong>
-                                                            </li>
-                                                            <li class="col-md-6 col-xs-12 icons-li" class="col-md-6 col-xs-12 icons-li">
-                                                                <strong><span style="color:#000000;">
-                                                                        <img style="margin: 0px 16px 0px -45px;" src="http://www.mosokna.ru/bitrix/templates/mosokna/images/icon_feat_like.png" style="margin: 0px 0px 0px -45px; padding: 0px;" /></span>
-                                                                    <a href="http://www.mosokna.ru/info/garantiya/" style="margin: 0px; padding: 0px; color: rgb(153, 153, 153);"><span style="color:#000000;">Гарантию высочайшего качестванашей продукции.</span></a></strong>
-                                                            </li>
-                                                        </ul>
-                                                    </div>
-                                                    <p> </p>
-                                                </div>
-                                            </div>
-                                        </div>
-
-                                        <div class="node-links">
-                                        </div>
-
-
-                                    </div>
-                                </div>
-                            </div>
-
-                        </div>
-                    </div>
-                    <div id="block-webform-client-block-8" class="block block-webform">
-                        <div class="block-inner">
-
-
-                            <div class="content">
-                                <form class="webform-client-form" enctype="multipart/form-data" action="/main"
-                                      method="post" id="webform-client-form-8" accept-charset="UTF-8">
-                                    <div>
-                                        <div class="form-item webform-component webform-component-textfield"
-                                             id="webform-component-name">
-                                            <input type="text" id="edit-submitted-name" name="submitted[name]" value="" size="60" maxlength="128" class="form-text"/>
-                                        </div>
-                                        <div class="form-item webform-component webform-component-textfield" id="webform-component-email">
-                                            <input type="text" id="edit-submitted-email" name="submitted[email]" value="" size="60" maxlength="128" class="form-text required"/>
-                                        </div>
-                                        <div class="form-item webform-component webform-component-textarea" id="webform-component-message">
-                                            <div class="form-textarea-wrapper"><textarea id="edit-submitted-message" name="submitted[message]" cols="60" rows="5" class="form-textarea"></textarea>
-                                            </div>
-                                        </div>
-                                        <input type="hidden" name="details[sid]" value=""/>
-                                        <input type="hidden" name="details[page_num]" value="1"/>
-                                        <input type="hidden" name="details[page_count]" value="1"/>
-                                        <input type="hidden" name="details[finished]" value="0"/>
-                                        <input type="hidden" name="form_build_id"
-                                               value="form--mntSv8xAby3DEvCkwXf_X0kyupsepeHAlDrCiRs-2c"/>
-                                        <input type="hidden" name="form_id" value="webform_client_form_8"/>
-
-                                        <div class="form-actions form-wrapper" id="edit-actions"><input
-                                                type="submit" id="edit-submit" name="op" value="Отправить"
-                                                class="form-submit"/></div>
-                                    </div>
-                                </form>
-                            </div>
-
-                        </div>
-                    </div>
-
+            <div class="twocolumns"  style="margin: 0px 0px 20px; padding: 0px; color: rgb(61, 61, 64); font-family: Calibri, Helvetica, Arial; font-size: 20px; line-height: 24px;">
+                <h3 class="h1" style="text-align:center; color:#ff0000">Мы рады предложить Вам</h3>
+                <ul style="padding-right: 0px; padding-left: 0px; list-style: none; zoom: 1;">
+                    <li class="col-md-6 col-xs-12 icons-li">
+                        <strong><span style="color:#000000;">
+                                <img style="margin-right:15px"
+                                                                  src="http://www.mosokna.ru/bitrix/templates/mosokna/images/icon_feat_photo.png"/>Современные пластиковые окна и балконные конструкции </span><a
+                                href="http://www.mosokna.ru/ceny/"
+                                style="margin: 0px; padding: 0px; color: rgb(51, 51, 51);"><span
+                                    style="color:#000000;">по доступным ценам</span></a><span
+                                style="color:#000000;">.</span></strong>
+                    </li>
+                    <li  class="col-md-6 col-xs-12 icons-li">
+                        <strong><span style="color:#000000;">
+                                <img style="margin-right:15px" src="http://www.mosokna.ru/bitrix/templates/mosokna/images/icon_feat_star.png"/>Квалифицированный подход к решению задач любой сложности.</span></strong>
+                    </li>
+                    <li class="col-md-6 col-xs-12 icons-li">
+                        <strong><span style="color:#000000;">
+                                <img style="margin-right:15px"  src="http://www.mosokna.ru/bitrix/templates/mosokna/images/icon_feat_brush.png" />Оперативный </span><a
+                                href="http://www.mosokna.ru/uslugi/montaj/" ><span
+                                    style="color:#000000;">монтаж пластиковых окон по ГОСТу</span></a><span
+                                style="color:#000000;">, который выполняется специалистами с солидным опытом.</span></strong>
+                    </li>
+                    <li class="col-md-6 col-xs-12 icons-li">
+                        <strong><span style="color:#000000;">
+                            <img style="margin-right:15px" src="http://www.mosokna.ru/bitrix/templates/mosokna/images/icon_feat_like.png"/></span>
+                            <a href="http://www.mosokna.ru/info/garantiya/" style="margin: 0px; padding: 0px; color: rgb(153, 153, 153);"><span style="color:#000000;">Гарантию высочайшего качестванашей продукции.</span></a></strong>
+                    </li>
+                </ul>
             </div>
         </div>
         <!-- /main-content -->
     </div>
     <!-- /columns -->
-
 </div>
 <!-- /page -->
-
-<div id="secondaryContent">
-
-
-</div>
