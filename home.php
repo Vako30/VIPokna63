7b3078c893219db6346caba38effa61f65c766c2
Smarter1 <vako3008@gmail.com>
2014-12-19 17:50:52 +0400

    +owl coursel

--- home.php
+++ home.php
@@ -11,32 +11,22 @@
 
 
                     <div class="content">
-                        <div id="secondaryPage">
-                            <p class="rtecenter"><span style="font-family:arial,helvetica,sans-serif;"><span style="font-size:14px;">О КОМПАНИИ</span></span></p>
-                            <p class="rtecenter"><span style="font-family:arial,helvetica,sans-serif;"><span
-                                        style="font-size:14px;">«VIP-ОКНА» — это производитель окон, который начал свое развитие в 1993 в Армении, и два года назад начали свою деятельность в городе Самара.</span></span><span
-                                    style="font-family: arial, helvetica, sans-serif; font-size: 14px;"> За это время мы успели заслужить доверие и уважение многих клиентов. Мы работаем качественно, выполняя даже самые сложные заказы точно в срок.</span>
-                            </p>
+                        <div class="col-xs-12">
+                            <div class="alert alert-danger" style="margin:10px 0" role="alert">
+                                <p class="rtecenter"><span style="font-family:arial,helvetica,sans-serif;"><span style="font-size:14px;">О КОМПАНИИ</span></span></p>
+                                <p class="rtecenter"><span style="font-family:arial,helvetica,sans-serif;"><span
+                                            style="font-size:14px;">«VIP-ОКНА» — это производитель окон, который начал свое развитие в 1993 в Армении, и два года назад начали свою деятельность в городе Самара.</span><br></span><span
+                                        style="font-family: arial, helvetica, sans-serif; font-size: 14px;"> За это время мы успели заслужить доверие и уважение многих клиентов. Мы работаем качественно, выполняя даже самые сложные заказы точно в срок.</span>
+                                </p>
+                            </div>
                         </div>
-                        <div class="rtecenter">
-                             
-                        </div>
-                        <div class="rtecenter">
-                            <span style="font-family:arial,helvetica,sans-serif;"><span style="font-size:14px;">Пластиковые окна на сегодняшний день являются незаменимым элементом дизайнерских решений, приносящим в дом солнечный свет,</span></span><span
-                                style="font-size: 14px; font-family: arial, helvetica, sans-serif;">тепло и уют.</span>
-                        </div>
-                        <div class="rtecenter">
-                            <span style="font-size: 14px; font-family: arial, helvetica, sans-serif;">На рынке оконной индустрии представлен широкий выбор разноплановых оконных конструкций.</span>
-                        </div>
-                        <div class="rtecenter">
-                            <span style="font-family:arial,helvetica,sans-serif;"><span style="font-size:14px;">Мы не устанавливаем дешевых окон.</span></span>
-                        </div>
-                        <div class="rtecenter">
-                            <span style="font-family:arial,helvetica,sans-serif;"><span style="font-size:14px;">Только надёжные, проверенные марки, но по приемлемым ценам. И не стоит забывать о технологии установки окон ПВХ - неправильный монтаж </span></span>
-                        </div>
-                        <div class="rtecenter">
-                            <span style="font-family:arial,helvetica,sans-serif;"><span style="font-size:14px;">сокращает срок службы даже идеально изготовленного окна. </span></span>
-                        </div>
+                        <p class="col-xs-12" style="text-align: center; margin:10px 0">
+                        Пластиковые окна на сегодняшний день являются незаменимым элементом дизайнерских решений, приносящим в дом солнечный свет,тепло и уют.
+                        На рынке оконной индустрии представлен широкий выбор разноплановых оконных конструкций.
+                        Мы не устанавливаем дешевых окон.
+                        Только надёжные, проверенные марки, но по приемлемым ценам. И не стоит забывать о технологии установки окон ПВХ - неправильный монтаж
+                        сокращает срок службы даже идеально изготовленного окна.
+                        </p>
                     </div>
 
                 </div>
@@ -46,27 +36,33 @@
 
 
                     <div class="content">
-                        <div class="box alone">
+                        <div class="alone" style="padding:10px 0; display:inline-block">
                             <div class="col-xs-12 col-md-4 what">
-                                <img style="margin:0 auto; float:none" alt="" src="<?php bloginfo('template_url'); ?>/assets/img/heart.png"/>
-                                <p style="font-size:14px; font-family:arial,helvetica,sans-serif;">
-                                    <strong><span class="h1">КАЧЕСТВО</span></strong>
+                                <div class="col-md-12"><img style="margin:0 auto; float:none" alt="" src="<?php bloginfo('template_url'); ?>/assets/img/heart.png"/></div>
+                                <div class="col-md-12">
+                                    <p style="font-size:14px; font-family:arial,helvetica,sans-serif;">
+                                    <span class="h1">КАЧЕСТВО</span><br>
                                     <span class="text">Гварантия высочайшего качества нашей продукции.</span>
-                                </p>
+                                    </p>
+                                </div>
                             </div>
                             <div class="col-xs-12 col-md-4 what">
-                                <img style="margin:0 auto; float:none" alt="" src="<?php bloginfo('template_url'); ?>/assets/img/calcul.png"/>
-                                <p style="font-size:14px; font-family:arial,helvetica,sans-serif;">
-                                    <strong><span class="h1">ЦЕНА</span></strong>
-                                    <span class="text">Приемлимые цены, и скидки для постоянных клиентов.</span>
-                                </p>
+                                <div class="col-md-12"><img style="margin:0 auto; float:none" alt="" src="<?php bloginfo('template_url'); ?>/assets/img/calcul.png"/></div>
+                                <div class="col-md-12">
+                                    <p style="font-size:14px; font-family:arial,helvetica,sans-serif;">
+                                        <span class="h1">ЦЕНА</span><br>
+                                        <span class="text">Приемлимые цены, и скидки для постоянных клиентов.</span>
+                                    </p>
+                                </div>
                             </div>
                             <div class="col-xs-12 col-md-4 what">
-                                <img style="margin:0 auto; float:none" alt="" src="<?php bloginfo('template_url'); ?>/assets/img/clock.png"/>
-                                <p style="font-size:14px; font-family:arial,helvetica,sans-serif;">
-                                    <strong><span class="h1">СЖАТЫЕ СРОКИ</span></strong>
-                                    <span class="text">Выполнение работ любой сложности в сжатые сроки.</span>
-                                </p>
+                                <div class="col-md-12"><img style="margin:0 auto; float:none" alt="" src="<?php bloginfo('template_url'); ?>/assets/img/clock.png"/></div>
+                                <div class="col-md-12">
+                                    <p style="font-size:14px; font-family:arial,helvetica,sans-serif;">
+                                        <span class="h1">СЖАТЫЕ СРОКИ</span><br>
+                                        <span class="text">Выполнение работ любой сложности в сжатые сроки.</span>
+                                    </p>
+                                </div>
                             </div>
                         </div>
                         <div style="clear:both"></div>
