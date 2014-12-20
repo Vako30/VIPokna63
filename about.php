66ff59367a465f88f64229ac5a0bc95235b266b1
VahanH <vahanius@gmail.com>
2014-12-20 16:57:14 +0400

    dock-front

--- about.php
+++ about.php
@@ -8,8 +8,7 @@ Template Name: about
         <div id="mainContent">
             <div class="local-tasks"></div>
             <div id="content">
-                <div class="region region-content">
-                    <div id="block-system-main" class="block block-system">
+                <div id="block-system-main" class="block block-system">
                         <div class="block-inner">
                             <div class="content">
                                 <div id="node-6" class="node node-page" about="/about" typeof="foaf:Document">
@@ -18,101 +17,66 @@ Template Name: about
                                             <div class="field field-name-body field-type-text-with-summary field-label-hidden">
                                                 <div class="field-item even" property="content:encoded">
                                                     <div class="out out-pattern" style="opacity: 1;">
-                                                        <div>
-                                                            <span style="font-family:arial,helvetica,sans-serif;">
-                                                                <span style="font-size:20px;">
-                                                                    <span style="color:#ff0000;">«VIP-ОКНА»</span>
-                                                                    — это производитель окон, который начал свое развитие в 1993 в Армении,
+                                                          <span style="color:#ff0000; font-size:24px;font-weight:bold;">«VIP-ОКНА»</span>
+                                                                   <span style="font-size:20px;"> — это производитель окон, который начал свое развитие в 1993 в Армении,
                                                                     и два года назад начали свою деятельность в городе Самара. За это время
-                                                                    мы успели заслужить доверие и уважение многих клиентов.
+                                                                    мы успели заслужить доверие и уважение многих клиентов.</span>
                                                                     <span style="color:#ff0000;"> Мы работаем качественно, выполняя даже самые сложные
                                                                         заказы точно в срок.</span>
-                                                                </span>
-                                                            </span>
-                                                        </div>
                                                         <p class="rtejustify">
-                                                            <span style="color:#000000;">
-                                                                <span style="font-family:arial,helvetica,sans-serif;">
-                                                                    <span style="font-size:20px;">
+
+                                                                <span style="font-size:20px;color:#000000;font-family:arial,helvetica,sans-serif;">
                                                                         <img alt="" src="<?php bloginfo('template_url'); ?>/assets/img/download.jpg" style="float: right; width: 292px; height: 299px; margin: 5px; border-width: 0px; border-style: solid;"/>
-                                                                    </span>
                                                                 </span>
-                                                            </span>
                                                         </p>
 
                                                         <p class="rtejustify">
-                                                            <span style="color:#000000;">
-                                                                <span style="font-size:20px;">
-                                                                    <span style="font-family:arial,helvetica,sans-serif;">Пластиковые окна на сегодняшний день являются
+
+                                                                    <span style="color:#000000;font-size:20px;font-family:arial,helvetica,sans-serif;">Пластиковые окна на сегодняшний день являются
                                                                         незаменимым элементом дизайнерских решений, приносящим в дом
                                                                         солнечный свет, тепло и уют. На рынке оконной индустрии
-                                                                        представлен широкий выбор разноплановых оконных конструкций.
-                                                                    </span>
-                                                                </span>
-                                                            </span>
-                                                            <span style="color:#ff0000;">
-                                                                <span style="font-size:20px;">
-                                                                    <span style="font-family:arial,helvetica,sans-serif;">Мы не устанавливаем дешевых окон.</span>
-                                                                </span>
-                                                            </span>
-                                                            <span style="color:#000000;">
-                                                                <span style="font-size:20px;">
-                                                                    <span style="font-family:arial,helvetica,sans-serif;"> Только надёжные, проверенные марки,
+                                                                        представлен широкий выбор разноплановых оконных конструкций.</span>
+
+                                                                    <span style="color:#ff0000;font-size:20px;font-family:arial,helvetica,sans-serif;">Мы не устанавливаем дешевых окон.</span>
+                                                                    <span style="font-family:arial,helvetica,sans-serif;font-size:20px;color:#000000;"> Только надёжные, проверенные марки,
                                                                         но по приемлемым ценам. И не стоит забывать о технологии
                                                                         установки окон ПВХ - неправильный монтаж сокращает срок
                                                                         службы даже идеально изготовленного окна. 
                                                                     </span>
-                                                                </span>
-                                                            </span>
                                                         </p>
                                                     </div>
-                                                    <div>
-                                                         
-                                                    </div>
-                                                    <p style="margin: 10px 0px 20px; padding: 0px; color: rgb(61, 61, 64); font-family: Calibri, Helvetica, Arial; font-size: 20px; line-height: 24px;">
+                                                   <p style="color: rgb(61, 61, 64); font-family: Calibri, Helvetica, Arial; font-size: 20px; line-height: 24px;">
                                                         <strong>
-                                                            <span style="color:#000000;">
-                                                                <span style="font-size:20px;">Мы рады предложить Вам:</span>
-                                                            </span>
+                                                            <span style="color:#000000;font-size:20px;">Мы рады предложить Вам:</span>
                                                         </strong>
                                                     </p>
 
-                                                    <p> </p>
 
-                                                    <div class="twocolumns"
-                                                         style="margin: 0px 0px 20px; padding: 0px; color: rgb(61, 61, 64); font-family: Calibri, Helvetica, Arial; font-size: 20px; line-height: 24px;">
+                                                    <div class="twocolumns"  style="margin: 0px 0px 20px; padding: 0px; color: rgb(61, 61, 64); font-family: Calibri, Helvetica, Arial; font-size: 20px; line-height: 24px;">
                                                         <ul style="margin: 28px -10px 20px -50px; padding-right: 0px; padding-left: 0px; list-style: none; zoom: 1;">
-                                                            <li style="margin: 10px 0px 20px 50px; padding: 0px 0px 0px 45px; width: 370px; float: left; font-size: 18px;">
+                                                            <li class="col-md-6 col-xs-12">
                                                                 <strong><span style="color:#000000;"><img
-                                                                            src="http://www.mosokna.ru/bitrix/templates/mosokna/images/icon_feat_photo.png"
-                                                                            style="margin: 0px 0px 0px -45px; padding: 0px; float: left;"/>Современные пластиковые окна и балконные конструкции </span><a
+                                                                            src="http://www.mosokna.ru/bitrix/templates/mosokna/images/icon_feat_photo.png"/>Современные пластиковые окна и балконные конструкции </span><a
                                                                         href="http://www.mosokna.ru/ceny/"
                                                                         style="margin: 0px; padding: 0px; color: rgb(51, 51, 51);"><span
                                                                             style="color:#000000;">по доступным ценам</span></a><span
                                                                         style="color:#000000;">.</span></strong>
                                                             </li>
-                                                            <li style="margin: 10px 0px 20px 50px; padding: 0px 0px 0px 45px; width: 370px; float: left; font-size: 18px;">
+                                                            <li  class="col-md-6 col-xs-12">
                                                                 <strong><span style="color:#000000;"><img
-                                                                            src="http://www.mosokna.ru/bitrix/templates/mosokna/images/icon_feat_star.png"
-                                                                            style="margin: 0px 0px 0px -45px; padding: 0px; float: left;"/>Квалифицированный подход к решению задач любой сложности.</span></strong>
+                                                                            src="http://www.mosokna.ru/bitrix/templates/mosokna/images/icon_feat_star.png"/>Квалифицированный подход к решению задач любой сложности.</span></strong>
                                                             </li>
-                                                            <li style="margin: 10px 0px 20px 50px; padding: 0px 0px 0px 45px; width: 370px; float: left; font-size: 18px;">
-                                                                <strong><span style="color:#000000;"><img
-                                                                            src="http://www.mosokna.ru/bitrix/templates/mosokna/images/icon_feat_brush.png"
-                                                                            style="margin: 0px 0px 0px -45px; padding: 0px; float: left;"/>Оперативный </span><a
+                                                            <li  class="col-md-6 col-xs-12">
+                                                                <strong><span style="color:#000000;"><img src="http://www.mosokna.ru/bitrix/templates/mosokna/images/icon_feat_brush.png" />Оперативный </span><a
                                                                         href="http://www.mosokna.ru/uslugi/montaj/"
                                                                         style="margin: 0px; padding: 0px; color: rgb(51, 51, 51);"><span
                                                                             style="color:#000000;">монтаж пластиковых окон по ГОСТу</span></a><span
                                                                         style="color:#000000;">, который выполняется специалистами с солидным опытом.</span></strong>
                                                             </li>
-                                                            <li style="margin: 10px 0px 20px 50px; padding: 0px 0px 0px 45px; width: 370px; float: left; font-size: 18px;">
-                                                                <strong><span style="color:#000000;"><img
-                                                                            src="http://www.mosokna.ru/bitrix/templates/mosokna/images/icon_feat_like.png"
-                                                                            style="margin: 0px 0px 0px -45px; padding: 0px; float: left;"/></span><a
-                                                                        href="http://www.mosokna.ru/info/garantiya/"
-                                                                        style="margin: 0px; padding: 0px; color: rgb(153, 153, 153);"><span
-                                                                            style="color:#000000;">Гарантию высочайшего качества</span></a><span
-                                                                        style="color:#000000;"> нашей продукции.</span></strong>
+                                                            <li class="col-md-6 col-xs-12">
+                                                                <strong><span style="color:#000000;">
+                                                                        <img src="http://www.mosokna.ru/bitrix/templates/mosokna/images/icon_feat_like.png"/></span>
+                                                                    <a href="http://www.mosokna.ru/info/garantiya/" style="margin: 0px; padding: 0px; color: rgb(153, 153, 153);"><span style="color:#000000;">Гарантию высочайшего качестванашей продукции.</span></a></strong>
                                                             </li>
                                                         </ul>
                                                     </div>
@@ -141,20 +105,13 @@ Template Name: about
                                     <div>
                                         <div class="form-item webform-component webform-component-textfield"
                                              id="webform-component-name">
-                                            <input type="text" id="edit-submitted-name" name="submitted[name]"
-                                                   value="" size="60" maxlength="128" class="form-text"/>
+                                            <input type="text" id="edit-submitted-name" name="submitted[name]" value="" size="60" maxlength="128" class="form-text"/>
                                         </div>
-                                        <div class="form-item webform-component webform-component-textfield"
-                                             id="webform-component-email">
-                                            <input type="text" id="edit-submitted-email" name="submitted[email]"
-                                                   value="" size="60" maxlength="128" class="form-text required"/>
+                                        <div class="form-item webform-component webform-component-textfield" id="webform-component-email">
+                                            <input type="text" id="edit-submitted-email" name="submitted[email]" value="" size="60" maxlength="128" class="form-text required"/>
                                         </div>
-                                        <div class="form-item webform-component webform-component-textarea"
-                                             id="webform-component-message">
-                                            <div class="form-textarea-wrapper"><textarea id="edit-submitted-message"
-                                                                                         name="submitted[message]"
-                                                                                         cols="60" rows="5"
-                                                                                         class="form-textarea"></textarea>
+                                        <div class="form-item webform-component webform-component-textarea" id="webform-component-message">
+                                            <div class="form-textarea-wrapper"><textarea id="edit-submitted-message" name="submitted[message]" cols="60" rows="5" class="form-textarea"></textarea>
                                             </div>
                                         </div>
                                         <input type="hidden" name="details[sid]" value=""/>
@@ -174,7 +131,6 @@ Template Name: about
 
                         </div>
                     </div>
-                </div>
 
             </div>
         </div>
@@ -184,3 +140,8 @@ Template Name: about
 
 </div>
 <!-- /page -->
+
+<div id="secondaryContent">
+
+
+</div>
